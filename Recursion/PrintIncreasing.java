da00c28fa12cb7ab0a50c63d25234fcba23a2ea5
Daman Preet Singh <51940359+damanpreet1234@users.noreply.github.com>
2021-06-01 22:47:20 +0530

    Print Increasing

---
+++ Recursion/PrintIncreasing.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PrintIncreasing{
+    public static Scanner scn = new Scanner(System.in);
+    public static void  increasing(int n)
+    {
+        if(n == 0) return ;
+        increasing(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args){
+        int n = scn.nextInt();
+        increasing(n);
+    }
+}
